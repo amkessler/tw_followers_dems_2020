bec20fd1497b038d2524b24ff9f245ed19a693f3
amkessler <kessler.aaron@gmail.com>
2019-06-24 15:06:17 -0400

    trying three test accounts

--- 00_sampletesting.R
+++ 00_sampletesting.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(UpSetR)
 
 # get a list of twitter handles you want to compare (I left out Hadley because everyone follows him)
-rstaters <- c("GovernorBullock")
+rstaters <- c("kyletblaine")
 
 # scrape the user_id of all followers for each handle in the list and bind into 1 dataframe
 followers <- map_df(rstaters, ~ get_followers(.x, n = 200000, retryonratelimit = TRUE) %>% mutate(account = .x))
@@ -16,16 +16,23 @@ followers <- map_df(rstaters, ~ get_followers(.x, n = 200000, retryonratelimit =
 head(followers)
 
 #save as RDS
-saveRDS(followers, "processed_data/followers_bullock.rds")
+saveRDS(followers, "processed_data/followers_kyleblaine.rds")
 
-# 
+#combine all the saved follower dfs into one
+file1 <- readRDS("processed_data/followers_kyleblaine.rds")
+file2 <- readRDS("processed_data/followers_davidwright.rds")
+file3 <- readRDS("processed_data/followers_davidchallian.rds")
+
+followers <- bind_rows(file1, file2, file3)
+
+
 # 
 # # get a de-duplicated list of all followers
 # aRdent_followers <- unique(followers$user_id)
 # 
 # # for each follower, get a binary indicator of whether they follow each tweeter or not and bind to one dataframe
-# binaries <- rstaters %>% 
-#   map_dfc(~ ifelse(aRdent_followers %in% filter(followers, account == .x)$user_id, 1, 0) %>% 
+# binaries <- rstaters %>%
+#   map_dfc(~ ifelse(aRdent_followers %in% filter(followers, account == .x)$user_id, 1, 0) %>%
 #             as.data.frame) # UpSetR doesn't like tibbles
 # 
 # # set column names
@@ -36,5 +43,5 @@ saveRDS(followers, "processed_data/followers_bullock.rds")
 # 
 # 
 # # plot the sets with UpSetR
-# upset(binaries, nsets = 7, main.bar.color = "SteelBlue", sets.bar.color = "DarkCyan", 
+# upset(binaries, nsets = 7, main.bar.color = "SteelBlue", sets.bar.color = "DarkCyan",
 #       sets.x.label = "Follower Count", text.scale = c(rep(1.4, 5), 1), order.by = "freq")
