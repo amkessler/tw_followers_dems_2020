e23c8449c4d014a341b2fa083b74d5ba23ea12b5
amkessler <kessler.aaron@gmail.com>
2019-06-24 13:50:01 -0400

    comment out comparison section for now

--- 00_sampletesting.R
+++ 00_sampletesting.R
@@ -5,10 +5,10 @@ library(tidyverse)
 library(UpSetR)
 
 # get a list of twitter handles you want to compare (I left out Hadley because everyone follows him)
-rstaters <- c("PeteButtigieg", "GovernorBullock")
+rstaters <- c("PeteButtigieg")
 
 # scrape the user_id of all followers for each handle in the list and bind into 1 dataframe
-followers <- map_df(rstaters, ~ get_followers(.x, n = 20000, retryonratelimit = TRUE) %>% mutate(account = .x))
+followers <- map_df(rstaters, ~ get_followers(.x, n = 2000000, retryonratelimit = TRUE) %>% mutate(account = .x))
 
 # We should now have a long 2 column dataframe with user_id (followers) and the hande of who 
 # they are following in another. Let’s take a peek.
@@ -16,25 +16,25 @@ followers <- map_df(rstaters, ~ get_followers(.x, n = 20000, retryonratelimit = 
 head(followers)
 
 #save as RDS
-saveRDS(followers, "processed_data/pete_and_bullock.rds")
+saveRDS(followers, "processed_data/followers_buttigieg.rds")
 
-
-
-# get a de-duplicated list of all followers
-aRdent_followers <- unique(followers$user_id)
-
-# for each follower, get a binary indicator of whether they follow each tweeter or not and bind to one dataframe
-binaries <- rstaters %>% 
-  map_dfc(~ ifelse(aRdent_followers %in% filter(followers, account == .x)$user_id, 1, 0) %>% 
-            as.data.frame) # UpSetR doesn't like tibbles
-
-# set column names
-names(binaries) <- rstaters
-
-# have a look at the data
-head(binaries)
-
-
-# plot the sets with UpSetR
-upset(binaries, nsets = 7, main.bar.color = "SteelBlue", sets.bar.color = "DarkCyan", 
-      sets.x.label = "Follower Count", text.scale = c(rep(1.4, 5), 1), order.by = "freq")
+# 
+# 
+# # get a de-duplicated list of all followers
+# aRdent_followers <- unique(followers$user_id)
+# 
+# # for each follower, get a binary indicator of whether they follow each tweeter or not and bind to one dataframe
+# binaries <- rstaters %>% 
+#   map_dfc(~ ifelse(aRdent_followers %in% filter(followers, account == .x)$user_id, 1, 0) %>% 
+#             as.data.frame) # UpSetR doesn't like tibbles
+# 
+# # set column names
+# names(binaries) <- rstaters
+# 
+# # have a look at the data
+# head(binaries)
+# 
+# 
+# # plot the sets with UpSetR
+# upset(binaries, nsets = 7, main.bar.color = "SteelBlue", sets.bar.color = "DarkCyan", 
+#       sets.x.label = "Follower Count", text.scale = c(rep(1.4, 5), 1), order.by = "freq")
